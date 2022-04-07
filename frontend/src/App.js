63096267460ea027a6aec015bb193384ccb123c7
Tawanda3012 <tawanda@uncommon.org>
2022-04-07 09:25:36 +0200

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
-import Navigation from './components/common/Navigation';
-import BookList from './components/books/BookList';
-// import ReviewList from './components/reviews/ReviewList';
 import './App.css';
+import BookList from './features/books/BookList';
+import Navigation from './features/common/Navigation';
+import ReviewList from './features/reviews/ReviewList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './features/about/About';
 
 function App() {
+
   return (
-    <div className="container-fluid">
-      <Navigation />
-      <div className="container">
-        <BookList />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<BookList />} />
+          <Route path="about" element={<About />} />
+          <Route path="/reviews/:bookId/:title" element={<ReviewList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
